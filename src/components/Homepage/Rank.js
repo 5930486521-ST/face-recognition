a33498ac28651dbf953d4c4ad1329a967aef6d57
gems.sirawit <sirawit.james@gmail.com>
2019-09-09 00:29:40 +0700

    feat: use aws lampda function (add emoji badge)

--- src/components/Homepage/Rank.js
+++ src/components/Homepage/Rank.js
@@ -1,9 +1,50 @@
 import React from "react";
 
-const Rank = ({userInfo}) =>
-    <div className='ma4 mt0 mb3 center'>
-        <p className='white f2 mt3 center'>{userInfo.name}, your current rank is...</p>
-        <p className='white f1 mb2 mt0 center'>#{userInfo.entries}</p>
-    </div>;
+const createEmojiMarkup = emoji => {
+  return { __html: emoji };
+};
+
+class Rank extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emoji: ""
+    };
+  }
+
+  setEmoji() {
+    fetch(
+      `https://fockjyzhr5.execute-api.us-east-1.amazonaws.com/prod/users/create?entries=${this.props.userInfo.entries}`
+    )
+      .then(res => res.json())
+      .then(({ emoji }) => this.setState({ emoji }))
+      .catch(err => this.setState({ emoji: "" }));
+  }
+
+  componentDidMount() {
+    this.setEmoji();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.userInfo.entries !== this.props.userInfo.entries) {
+      this.setEmoji();
+    }
+  }
+
+  render() {
+    const { userInfo } = this.props;
+    return (
+      <div className="ma4 mt0 mb3 center">
+        <p className="white f2 mt3 center">
+          {userInfo.name}, your current rank is...
+        </p>
+        <div className="white f1 mb2 mt0" style={{ textAlign: "center" }}>
+          #{userInfo.entries}{" "}
+          <span dangerouslySetInnerHTML={createEmojiMarkup(this.state.emoji)} />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Rank;
